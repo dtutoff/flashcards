25527d3e063c26bab6d409e214b0390c68ab3f6d
Denis Tutov <denistutov@hotmail.com>
2024-07-30 09:55:37 +0300

    feat: update user model validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,8 @@ class User < ApplicationRecord
   has_secure_password
   has_many :decks,  dependent: :destroy
 
-  validates :email, presence: true
-  validates :name, presence: true, length: { minimum: 2 }
+  before_save { email.downcase! }
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, length: { maximum: 100 }, format: { with: VALID_EMAIL_REGEX }, uniqueness: true
+  validates :name, presence: true, length: { minimum: 2, maximum: 50 }
 end
